e9c59b124d3640cf8ac77fa509adf521f577d34f
DanMarzo <marzogildan@gmail.com>
2024-03-10 14:28:09 -0300

    Exclusao de controller default e inclusao de controller de inclusao de controller para libreria

---
+++ TiendaServico.API.Libro/Controllers/LibroMaterialController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TiendaServico.API.Libro.Application;
+using TiendaServico.API.Libro.Models;
+
+namespace TiendaServico.API.Libro.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class LibroMaterialController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public LibroMaterialController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPost]
+    public async Task<ActionResult<LibreriaMaterial>> Crear(Nuevo.Ejecuta ejecuta)
+    {
+        var libreriaMaterial=  await _mediator.Send(ejecuta);
+        return Ok(libreriaMaterial);
+    }
+}
