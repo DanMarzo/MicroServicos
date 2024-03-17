31e318a5ff676680576c2ac1d3c09fcc40f54f73
DanMarzo <marzogildan@gmail.com>
2024-03-16 23:15:35 -0300

    Preparando Testes unitarios com Mock;

---
+++ TiendaServico.API.Libro.Test/LibroServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Moq;
+using TiendaServico.API.Libro.Application;
+using TiendaServico.API.Libro.Persistence;
+
+namespace TiendaServico.API.Libro.Test;
+
+public class LibroServiceTest
+{
+
+
+    [Fact]
+    public void GetLibros()
+    {
+        var moqContext = new Mock<ContextLibreria>();
+        var moqMapper = new Mock<IMapper>();
+        Consulta.Manejador manejador = new Consulta.Manejador(moqContext.Object, moqMapper.Object);
+    }
+}
